4c326ed7ee8b6aa462cc5bcdd29b421b8486b89d
Ryan Pham <phuc.pham.itf@gmail.com>
2024-06-06 14:46:36 +0800

    [Server] Update authentication

--- grpc-server/src/main/java/spring_boot/grpc/config/LoggingInterceptor.java
+++ grpc-server/src/main/java/spring_boot/grpc/config/LoggingInterceptor.java
@@ -3,45 +3,57 @@ package spring_boot.grpc.config;
 import io.grpc.*;
 
 public class LoggingInterceptor implements ServerInterceptor {
+	private static final String AUTH_TOKEN = "valid-token";
 
 	@Override
 	public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
 		ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
 
-		return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(
-			new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
+		String methodName = call.getMethodDescriptor().getFullMethodName();
+		String token = headers.get(Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER));
+		System.out.println("Received call for method: " + methodName);
+
+		if (AUTH_TOKEN.equals(token)) {
+			System.out.println("Authentication successful");
+			return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(
+				new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
+
+					@Override
+					public void sendMessage(RespT message) {
+						System.out.println("Sending response: " + message);
+						super.sendMessage(message);
+					}
+
+				}, headers)) {
 
 				@Override
-				public void sendMessage(RespT message) {
-					System.out.println("Sending response: " + message);
-					super.sendMessage(message);
+				public void onMessage(ReqT message) {
+					System.out.println("Received message: " + message);
+					super.onMessage(message);
 				}
 
-			}, headers)) {
+				@Override
+				public void onHalfClose() {
+					System.out.println("Half close received");
+					super.onHalfClose();
+				}
 
-			@Override
-			public void onMessage(ReqT message) {
-				System.out.println("Received message: " + message);
-				super.onMessage(message);
-			}
+				@Override
+				public void onComplete() {
+					System.out.println("Call complete");
+					super.onComplete();
+				}
 
-			@Override
-			public void onHalfClose() {
-				System.out.println("Half close received");
-				super.onHalfClose();
-			}
-
-			@Override
-			public void onComplete() {
-				System.out.println("Call complete");
-				super.onComplete();
-			}
-
-			@Override
-			public void onCancel() {
-				System.out.println("Call cancelled");
-				super.onCancel();
-			}
-		};
+				@Override
+				public void onCancel() {
+					System.out.println("Call cancelled");
+					super.onCancel();
+				}
+			};
+		} else {
+			System.out.println("Authentication failed");
+			call.close(Status.UNAUTHENTICATED.withDescription("Invalid auth token"), headers);
+			return new ServerCall.Listener<ReqT>() {};
+		}
 	}
 }

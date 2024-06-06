15a0dd37b762ff9693e06550cebfac2b0a40805b
Ryan Pham <phuc.pham.itf@gmail.com>
2024-06-06 11:36:22 +0800

    [Server] update interceptor to intercept sending response and receiving message

--- grpc-server/src/main/java/spring_boot/grpc/config/LoggingInterceptor.java
+++ grpc-server/src/main/java/spring_boot/grpc/config/LoggingInterceptor.java
@@ -8,32 +8,39 @@ public class LoggingInterceptor implements ServerInterceptor {
 	public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
 		ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
 
-		return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(call, headers)) {
+		return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(
+			new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
+
+				@Override
+				public void sendMessage(RespT message) {
+					System.out.println("Sending response: " + message);
+					super.sendMessage(message);
+				}
+
+			}, headers)) {
 
 			@Override
 			public void onMessage(ReqT message) {
-				System.out.println("Receive Request message: " + message.toString());
+				System.out.println("Received message: " + message);
 				super.onMessage(message);
 			}
 
 			@Override
 			public void onHalfClose() {
+				System.out.println("Half close received");
 				super.onHalfClose();
 			}
 
 			@Override
-			public void onCancel() {
-				super.onCancel();
-			}
-
-			@Override
 			public void onComplete() {
+				System.out.println("Call complete");
 				super.onComplete();
 			}
 
 			@Override
-			public void onReady() {
-				super.onReady();
+			public void onCancel() {
+				System.out.println("Call cancelled");
+				super.onCancel();
 			}
 		};
 	}
